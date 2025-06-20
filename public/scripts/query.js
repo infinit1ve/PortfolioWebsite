63e9e0227c4dbd7bdefa0918e780f5f55f86d6b9
infinit1ve <mtaraschan@gmail.com>
2025-06-20 22:47:37 +0200

    Add dynamic message for blog redirection; update query.js for German language support

--- public/scripts/query.js
+++ public/scripts/query.js
@@ -6,12 +6,14 @@ document.addEventListener('DOMContentLoaded', function() {
     en: {
       '4460': `<p>You scanned my card! Feel free to explore or reach out at <a href="mailto:contact@infinitive.cc">contact@infinitive.cc</a></p>`,
       '5658': `<p>You probably came here for my CV &#8212; everything you need is <a href="/en/CV.pdf">here</a>!</p>`,
-      '7426': `<p>Found something I made? This is where it all ties together</p>`
+      '7426': `<p>Found something I made? This is where it all ties together</p>`,
+      '3267': `<p>Es tut mir Leid, aber der Blog ist derzeit nur auf Englisch verfügbar</p>`
     },
     de: {
       '4460': `<p>Du hast meine Visitenkarte gescannt! Schau dich um oder kontaktiere mich unter <a href="mailto:contact@infinitive.cc">contact@infinitive.cc</a></p>`,
       '5658': `<p>Sie sind wahrscheinlich wegen meines Lebenslaufs hierher gekommen &#8212; alles, was Sie brauchen, ist <a href="/de/Lebenslauf.pdf">hier</a>!</p>`,
-      '7426': `<p>Hast du etwas gefunden, das ich gemacht habe? Hier kommt alles zusammen</p>`
+      '7426': `<p>Hast du etwas gefunden, das ich gemacht habe? Hier kommt alles zusammen</p>`,
+      '3267': `<p>Es tut mir Leid, aber der Blog ist derzeit nur auf Englisch verfügbar</p>`
     }
   }
 
