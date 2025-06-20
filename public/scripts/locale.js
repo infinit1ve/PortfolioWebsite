618ac09f7388e96eca24ada5872f573ebd1ff68c
infinit1ve <mtaraschan@gmail.com>
2025-06-20 22:24:14 +0200

    Enhance website messaging and navigation; add dynamic message display based on URL parameters and improve redirection logic

--- public/scripts/locale.js
+++ public/scripts/locale.js
@@ -1,6 +1,7 @@
 const localeSelector = document.getElementById("localeSelector");
 const locale = localeSelector.value;
+const currentURI = window.location.pathname + window.location.search;
 
 localeSelector.addEventListener('change', function() {
-    window.location.href = `/${this.value}/`;
+    window.location.href = `/${this.value}/${currentURI.slice(4)}`;
 });
---
+++ public/scripts/query.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const params = new URLSearchParams(window.location.search);
+  const msgid = params.get('msg');
+  const msgBox = document.getElementById('message');
+  const msgMap = {
+    en: {
+      '4460': `<p>You scanned my card! Feel free to explore or reach out at <a href="mailto:contact@infinitive.cc">contact@infinitive.cc</a></p>`,
+      '5658': `<p>You probably came here for my CV &#8212; everything you need is <a href="/en/CV.pdf">here</a>!</p>`,
+      '7426': `<p>Found something I made? This is where it all ties together</p>`
+    },
+    de: {
+      '4460': `<p>Du hast meine Visitenkarte gescannt! Schau dich um oder kontaktiere mich unter <a href="mailto:contact@infinitive.cc">contact@infinitive.cc</a></p>`,
+      '5658': `<p>Sie sind wahrscheinlich wegen meines Lebenslaufs hierher gekommen &#8212; alles, was Sie brauchen, ist <a href="/de/Lebenslauf.pdf">hier</a>!</p>`,
+      '7426': `<p>Hast du etwas gefunden, das ich gemacht habe? Hier kommt alles zusammen</p>`
+    }
+  }
+
+  if (msgid) {
+    msgBox.innerHTML = msgMap[`${locale}`][`${msgid}`];
+    msgBox.style.display = "";
+  } else {
+    msgBox.innerHTML = `<p>The website is currently under construction, expect changes, broken links, and placeholder content</p>`;
+    msgBox.style.display = "";
+  }
+});
