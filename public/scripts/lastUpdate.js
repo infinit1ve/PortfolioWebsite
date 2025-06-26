d799758635c708ddc9f22be2fce43a9b45e36675
infinit1ve <mtaraschan@gmail.com>
2025-06-26 19:57:11 +0200

    Update footer to include dynamic copyright year and last updated date

--- public/scripts/lastUpdate.js
+++ public/scripts/lastUpdate.js
@@ -1,12 +1,15 @@
-const dateText = document.getElementById('date-text')
+const dateText = document.getElementById('date-text');
+const copyrightYearText = document.getElementById('copyright-year');
 
 document.addEventListener('DOMContentLoaded', function() {
   fetch('../api/lastUpdate')
   .then(response => response.json())  // parse JSON from the response
   .then(lastPush => {
     const DateTime = luxon.DateTime;
-    lastUpdate = DateTime.fromISO(lastPush).setLocale(locale).toLocaleString(DateTime.DATE_FULL);
+    const lastUpdate = DateTime.fromISO(lastPush).setLocale(locale).toLocaleString(DateTime.DATE_FULL);
     dateText.innerText = lastUpdate;
+    const currentYear = DateTime.fromISO(lastPush).toFormat('yyyy');
+    copyrightYearText.innerText = currentYear;
   })
   .catch(error => {
     console.error('Error fetching last update:', error);
