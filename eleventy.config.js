ebc650a109f4850dffff5486d0de3a3782256992
infinit1ve <mtaraschan@gmail.com>
2025-06-27 21:56:06 +0200

    Refactor blog structure: update input directory, add recent posts section, and implement new blog styles

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,7 @@ const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
 	// Order matters, put this at the top of your configuration file.
-  eleventyConfig.setInputDirectory("rawarticles");
+  eleventyConfig.setInputDirectory("src");
   eleventyConfig.setOutputDirectory("./public/en/blog/article");
   eleventyConfig.addFilter("readableDate", (dateObj, format = "d LLLL, yyyy") => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(format);
